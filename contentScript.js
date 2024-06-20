d374b2d153bd64503beadaf2d5261d13945c65e9
Jasons1142 <jasons11@bu.edu>
2024-06-20 17:46:32 -0400

    Updated contentScript.js with code

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,15 @@
 alert('Hello')
+function extractScores() {
+    const scoreElements = document.getElementsByClassName("submissionStatus--Score");
+    const scores = Array.from(scoreElements).map(el=>parseFloat(el.innerText));
+    return scores;
+}
+
+function calculateAverage(scores) {
+    if (scores.length == 0) return 0;
+    const sum = scores.reduce((acc, score) => acc + score, 0);
+    return sum/scores.length;
+}
+
+const scores = extractScores();
+const average = calculateAverage(scores);
