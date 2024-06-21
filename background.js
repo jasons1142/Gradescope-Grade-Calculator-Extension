3e2dce1171b80a2d5d1da7afb65a6536928e437e
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-21 16:44:08 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.from === 'content' && !message.isOnGradescope) {
-    //if we are not on gradescope we want to change what our extension looks like
-    document.getElementsByClassName("extension")[0].innerHTML = '<h1>' + 'Uh oh, looks like you\'re not on a Gradescope!' + '</h1>' + '<p>' + 'To calculate your grade, navigate to one of your courses on Gradescope.' + '</p>'
+    //if we are not on gradescope send message to popup.js
+     chrome.runtime.sendMessage({
+      from: 'background',
+      action: 'updatePopup',
+      message: 'Not on Gradescope'
+    });
   }
 });
