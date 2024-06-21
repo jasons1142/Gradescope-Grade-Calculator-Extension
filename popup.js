5b36da128806ebb6d7bf2ab667eb0c4e4ac386df
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-21 09:41:20 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,13 +1,19 @@
+
+
 function displayAverage(average) {
     document.getElementById('average').textContent = "Average of scores: " + average;
 }
 
+//send message to tab
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { action: "getScores" }, (response) => {
-      if (response) {
-        displayAverage(response.average);
-      } else {
-        document.getElementById('average').textContent = "No scores found.";
-      }
+      //if (response) {
+        //displayAverage(response.average);
+      //} else {
+        //document.getElementById('average').textContent = "No scores found.";
+      //}
     });
 });
+
+//check if we received message
+chrome.runtime.onMessage.addListener()
