e20ea1b7265670622e82f197e27a54bf82da169c
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-21 15:10:08 -0500

    Update popup.js
    working on accessing data from gradescope

--- popup.js
+++ popup.js
@@ -1,20 +1,21 @@
+//load in content
+document.addEventListener("DOMContentLoaded", function(){getData()}); //once loaded in get the data from gradescope
 
+//function displayAverage(average) {
+    //document.getElementById('average').textContent = "Average of scores: " + average;
+//}
 
-function displayAverage(average) {
-    document.getElementById('average').textContent = "Average of scores: " + average;
-}
-
-//send message to tab so that we may get data
+//send message to gradescope so that we may get data
 function getData(){
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "getScores" }, (response) => {
-      //if (response) {
-        //displayAverage(response.average);
-      //} else {
-        //document.getElementById('average').textContent = "No scores found.";
-      //}
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
+        let message{
+            txt: "getmydata"
+        }
+        chrome.tabs.sendMessage(tabs[0].id, message); //send message to current window
     });
-});
 }
-//check if we received message
-chrome.runtime.onMessage.addListener()
+
+//check if we received message and if we did call function
+chrome.runtime.onMessage.addListener(receivedMessage);
+
+function receivedMessage(){};
