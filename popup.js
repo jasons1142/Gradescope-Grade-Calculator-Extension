c048a3f9259b021ae614e9720eea9d4aa72c8157
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-21 16:17:01 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -18,9 +18,25 @@ function getData(){
 //check if we received message and if we did call function
 chrome.runtime.onMessage.addListener(receivedMessage);
 
-function receivedMessage(message, sender, sendResponse){
+function receivedMessage(request, sender, sendResponse){
 
     //if (request.length != # assignments) {
-        //document.getElementsByClassName
+        //document.getElementsByTagName('p') += '<br>' + '<p>' + 'Looks like you have some ungraded assignments. Fill in the text box to estimate your grade!' + '</p>'
+        //allGraded = request
+        //unGraded = .......
+        //checkBoxes(allGraded) // call function to create checkboxes for all the graded assignments
+        //textBoxes(unGraded) //call function to create textboxes for all  ungraded assignments
     //}
+    //else{
+    //allGraded = request 
+    //checkBoxes(alldata) //create checkboxes for all the graded assignments
+    //}
+};
+
+function checkBoxes(grades){
+    
+};
+
+function textBoxes(grades){
+    
 };
