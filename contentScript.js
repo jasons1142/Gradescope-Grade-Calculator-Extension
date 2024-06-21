004b0d20ca7045ac748e1050cdd0800f2730dad3
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-21 16:27:15 -0500

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,26 @@
-alert('Hello')
-function extractScores() {
-    const scoreElements = document.getElementsByClassName("submissionStatus--score");
-    const scores = Array.from(scoreElements).map(el=>parseFloat(el.innerText));
-    return scores;
+//alert('Hello')
+//function extractScores() {
+    //const scoreElements = document.getElementsByClassName("submissionStatus--score");
+    //const scores = Array.from(scoreElements).map(el=>parseFloat(el.innerText));
+    //return scores;
+//}
+
+//function calculateAverage(scores) {
+   // if (scores.length == 0) return 0;
+    //const sum = scores.reduce((acc, score) => acc + score, 0);
+    //return sum/scores.length;
+//}
+
+//const scores = extractScores();
+//const average = calculateAverage(scores);
+
+//function for finding if extension is running on gradescope.com/courses
+function isOnGradescope(url) {
+  return url.includes("gradescope.com/courses");
 }
 
-function calculateAverage(scores) {
-    if (scores.length == 0) return 0;
-    const sum = scores.reduce((acc, score) => acc + score, 0);
-    return sum/scores.length;
-}
-
-const scores = extractScores();
-const average = calculateAverage(scores);
+// Sending a message to the extension with result
+chrome.runtime.sendMessage({
+  from: 'content',
+  isOnGradescope: isOnGradescope(window.location.href)
+});
