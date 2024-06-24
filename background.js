ee2d9db0005afe993f8abec0d2ddd15432bc8b18
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-24 14:20:22 -0500

    Update background.js

--- background.js
+++ background.js
@@ -7,3 +7,19 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         }
     }
 });
+
+//if we received a message from contentscript we are on a new tab and need to update popup
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.from === 'content') {
+        updatePopup(message.isOnGradescope);
+    }
+});
+
+//check url of website when popup first appears
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { from: 'background' }, function(response) {
+        if (response && response.isOnGradescope !== undefined) {
+            updatePopup(response.isOnGradescope);
+        }
+    });
+});
