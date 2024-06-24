7c5a3ea583d18a4eb0a5da0016297445a7c7fbcd
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-24 13:29:12 -0500

    setting conditional statements for popup

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.from === 'content' && !message.isOnGradescope) {
-    //if we are not on gradescope send message to popup.js
-     chrome.runtime.sendMessage({
-      from: 'background',
-      action: 'updatePopup',
-      message: 'Not on Gradescope'
-    });
-  }
+    if (message.from === 'content') {
+        if (message.isGradescopeCourses) {
+            chrome.action.setPopup({ popup: 'popup.html' });
+        } else {
+            chrome.action.setPopup({ popup: 'NotOnGradescopePopup.html' });
+        }
+    }
 });
