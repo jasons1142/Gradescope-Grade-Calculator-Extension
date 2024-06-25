abfb94a317ba1414a8aa223f094cfdcdb7968b81
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-25 09:25:05 -0500

    checking if we are changing tabs

--- background.js
+++ background.js
@@ -7,3 +7,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         }
     }
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    if (changeInfo.status === 'complete'){
+        if (tab.url && tab.url.includes("gradescope.com/courses")){
+            chrome.action.setPopup({popup: 'popup.html'});
+        } else{
+            chrome.action.setPopup({popup: 'NotOnGradescope.html});
+        }
+    }
+});
