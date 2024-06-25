bcfe4a064ec4e4873ca1b9cdda5b11615f9b2e03
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-25 09:53:11 -0500

    Update background.js

--- background.js
+++ background.js
@@ -8,3 +8,12 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     }
 });
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    if (changeInfo.url){ //if the url has changed
+        if (tab.url && tab.url.includes("gradescope.com/courses")){ //if the url is in gradescope
+            chrome.action.setPopup({popup: 'popup.html'}); //set the correct popup
+        } else{
+            chrome.action.setPopup({popup: 'NotOnGradescope.html'}); //set the error popup
+        }
+    }
+});
