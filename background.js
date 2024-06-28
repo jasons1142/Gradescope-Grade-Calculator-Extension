269bbcee5185be1a1f9ccb56d1a2de1c10739152
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-28 11:38:22 -0500

    checking to see if the tab is on gradescope.com/courses

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.from === 'content') {
-        if (message.isOnGradescope) {
+        if (message.isOnGradescope && message.isOnGradescope.includes("gradescope.com/courses")) {
             chrome.action.setPopup({ popup: 'popup.html' });
         } else {
             chrome.action.setPopup({ popup: 'NotOnGradescopePopup.html' });
