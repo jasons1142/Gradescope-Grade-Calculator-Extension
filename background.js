4da1d73eb21cd3bdcd4bc17d8cd2c79330e9755d
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-28 13:16:58 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,1 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) { //changing the popup based on what tab we are on
-  if (message.from === 'content' && message.url) {
-    if (message.url.includes("gradescope.com/courses")) {
-      chrome.action.setPopup({ popup: 'popup.html' });
-    } else {
-      chrome.action.setPopup({ popup: 'NotOnGradescopePopup.html' });
-    }
-  }
-});
 
