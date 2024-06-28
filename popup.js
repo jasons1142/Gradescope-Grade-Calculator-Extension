7a56efd63606237f8dd59222e240b2f4afd70079
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-28 12:00:19 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -31,7 +31,7 @@ chrome.tabs.query({active: true, currentWindow: true }, (tabs) => {
   chrome.tabs.sendMessage(tabs[0].id, {action: "getAssignments"}, (response) => { //send message to contentscript so we can extract data
     if (chrome.runtime.lastError) { //error case
       console.error('Error sending message:', chrome.runtime.lastError);
-      document.getElementById('NotOnGradescope').textContent = "Uh oh, looks like you're not on Gradescope. To calculate your average, navigate to a course on Gradescope and refresh!";
+      chrome.action.setPopup({ popup: 'NotOnGradescopePopup.html' });
       return;
     }
 
