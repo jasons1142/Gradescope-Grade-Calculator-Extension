7e9f1077d6b48aca4bb9441b7f279ec3287f7fca
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-06-28 10:28:50 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,26 +1,6 @@
 //load in content
 document.addEventListener("DOMContentLoaded", function(){getData()}); //once loaded in get the data from gradescope
 
-//function displayAverage(average) {
-    //document.getElementById('average').textContent = "Average of scores: " + average;
-//}
-
-
-function receivedMessage(request, sender, sendResponse){
-
-    //if (request.length != # assignments) {
-        //document.getElementsByTagName('p') += '<br>' + '<p>' + 'Looks like you have some ungraded assignments. Fill in the text box to estimate your grade!' + '</p>'
-        //allGraded = request
-        //unGraded = .......
-        //checkBoxes(allGraded) // call function to create checkboxes for all the graded assignments
-        //textBoxes(unGraded) //call function to create textboxes for all  ungraded assignments
-    //}
-    //else{
-    //allGraded = request 
-    //checkBoxes(alldata) //create checkboxes for all the graded assignments
-    //}
-};
-
  function createAssignmentCheckboxes(assignments) {
   const container = document.getElementById('assignments-container');
   container.innerHTML = '';
@@ -49,7 +29,7 @@ chrome.tabs.query({active: true, currentWindow: true }, (tabs) => {
     return;
   }
 
-  chrome.tabs.sendMessage(tabs[0].id, {action: "getAssignments"}, (response) => { //send message to background ao we can extract data
+  chrome.tabs.sendMessage(tabs[0].id, {action: "getAssignments"}, (response) => { //send message to contentscript so we can extract data
     if (chrome.runtime.lastError) { //error case
       console.error('Error sending message:', chrome.runtime.lastError);
       document.getElementById('assignments-container').textContent = "Error retrieving assingments.";
