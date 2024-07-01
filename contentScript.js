3c369767b070e189ceddadca5472ee19d844778f
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-07-01 16:52:05 -0500

    deleting old comments

--- contentScript.js
+++ contentScript.js
@@ -39,13 +39,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 
-//function calculateAverage(scores) {
-   // if (scores.length == 0) return 0;
-    //const sum = scores.reduce((acc, score) => acc + score, 0);
-    //return sum/scores.length;
-//}
-
-//const scores = extractScores();
-//const average = calculateAverage(scores);
-
-
