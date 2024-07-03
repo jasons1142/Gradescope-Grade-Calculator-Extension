a3567bffe3a835a1b261dc5c0af7c92620599511
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-07-03 13:20:44 -0500

    updated variable to not just local scope

--- contentScript.js
+++ contentScript.js
@@ -9,7 +9,8 @@ function extractAssignments() {
         const scoreElement = row.querySelector('td.submissionStatus div.submissionStatus--score'); //get the score received on the assingment
         if (nameElement) { //checking to see if the name element exists
             const name = nameElement.innerText.trim(); 
-            let score = null;
+            let numerator = null; //initialize variable
+            let denominator = null; //initialize variable
 
             if (scoreElement) {
             const scoreText = scoreElement.innerText.trim(); //trimming any extra space in the fractional score
@@ -18,9 +19,9 @@ function extractAssignments() {
                     if (parts.length === 2) { //checking to make sure that parts has two elements
                         const numerator = parseFloat(parts[0].trim()); //making the numerator the first part that was split
                         const denominator = parseFloat(parts[1].trim());//making the denominator the second part that was split
-                        if (!isNaN(denominator) && !isNaN(numerator) && denominator !== 0) { //making sure the numerator and denominator are numbers and the denominator is not 0
-                            score = (numerator/denominator) * 100; //making the score the quotient of numerator/denominator
-                        }
+                        //if (!isNaN(denominator) && !isNaN(numerator) && denominator !== 0) { //making sure the numerator and denominator are numbers and the denominator is not 0
+                            //score = (numerator/denominator) * 100; //making the score the quotient of numerator/denominator
+                        //}
                     }
                 }
             }
